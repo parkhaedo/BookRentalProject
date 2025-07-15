63a35a0bbe275fa63907eec2895cda471197e631
parkhaedo <jobkid4195@gmail.com>
2025-07-15 22:58:23 +0900

    책 파일 관련 컨트롤러, 서비스 수정(書籍ファイル関連のコントローラーおよびサービスの修正 )

--- src/main/java/com/office/library/book/admin/BookController.java
+++ src/main/java/com/office/library/book/admin/BookController.java
@@ -3,16 +3,50 @@ package com.office.library.book.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.office.library.book.admin.util.UploadFileService;
+import com.office.library.book.BookVo;
 
 @Controller
 @RequestMapping("/book/admin")
 public class BookController {
+	
+	@Autowired
+	BookService bookService;
+	
+	@Autowired
+	UploadFileService uploadFileService;
 	
 	//도서등록
 	@GetMapping("/registerBookForm")
 	public String registerBookForm() {
 		System.out.println("[Bookcontroller] registerBookForm()");
 		String nextPage = "admin/book/register_book_form";
+		return nextPage;
+	}
+	
+	//도서등록확인
+	@PostMapping("/registerBookConfirm")
+	public String registerBookConfirm(BookVo bookVo,
+										@RequestParam("file") MultipartFile file) {
+		System.out.println("[Bookcontriller] registerBookConfirm");
+		String nextPage = "admin/book/register_book_ok";
+		
+		String savedFileName = uploadFileService.upload(file);
+		
+		if(savedFileName != null) {
+			bookVo.setB_thumbnail(savedFileName);
+			int result = bookService.registerBookConfirm(bookVo);
+			
+			if(result <=0)
+				nextPage = "admin/book/register_book_ng";
+		}else {
+			nextPage = "admin/book/register_book_ng";
+		}
 		return nextPage;
 	}
 }
--- src/main/java/com/office/library/book/admin/BookService.java
+++ src/main/java/com/office/library/book/admin/BookService.java
@@ -1,8 +1,34 @@
 package com.office.library.book.admin;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.office.library.book.BookVo;
 
 @Service
 public class BookService {
+	
+	final static public int BOOK_ISBN_ALREADY_EXIST = 0;
+	final static public int BOOK_REGISTER_SUCCESS = 1;
+	final static public int BOOK_REGISTER_FAIL = -1;
 
+	@Autowired
+	BookDao bookDao;
+	
+	public int registerBookconfirm(BookVo bookVo) {
+		System.out.println("[BookService] registerBookConfirm()");
+		
+		boolean isISBN = bookDao.isISBN(bookVo.getB_isbn());
+		
+		if(!isISBN) {
+			int result = bookDao.insertBook(bookVo);
+			
+			if(result>0)
+				return BOOK_REGISTER_SUCCESS;
+			else
+				return BOOK_REGISTER_FAIL;
+		}else {
+			return BOOK_ISBN_ALREADY_EXIST;
+		}
+	}
 }
